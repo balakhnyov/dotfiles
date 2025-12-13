89f8083335e4c10e009fca8da33c31624aa60147
balakhnyov <k.balakhnyov@gmail.com>
2025-12-14 01:21:23 +0700

    fix: utc-8 encoding added

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Кодировка
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- Формат файлов (Unix LF)
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = "unix,dos"
